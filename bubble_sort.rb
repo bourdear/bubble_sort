ae11fb6f6ecc9d8d128f07c8659e2d4c3b4a3a17
Ronnie <bourdear@gmail.com>
2020-07-18 19:39:51 -0700

    first commit

---
+++ bubble_sort.rb
@@ -0,0 +1,21 @@
+def bubble_sort(arr)
+  arr_length = arr.size
+  return arr if arr_length <= 1
+
+  loop do 
+    swapped = false
+    (arr_length-1).times do |i|
+      if arr[i] > arr[i+1]
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        swapped = true
+    end
+  end
+  break if not swapped
+end
+
+arr 
+end
+
+
+print bubble_sort([4,3,78,2,0,2])
+# => [0, 2, 2, 3, 4, 78]
